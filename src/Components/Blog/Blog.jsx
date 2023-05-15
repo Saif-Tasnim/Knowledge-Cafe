e5176f99dd05e0dc50530300496b46e882ca5458
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-05-15 19:45:57 +0600

    blogs part complete

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,19 +1,55 @@
 import React from 'react';
 import './Blog.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Blog = (props) => {
-    console.log(props.blog);
-    const {id, author_name , blog_title, Blog_cover_image, Author_image, Read_time, Publish_Date} = props.blog;
-     
-    return (
-        <div>
-            <h1>{author_name}</h1>
-            <h1>{blog_title}</h1>
-            <h1>{Read_time}</h1>
-            <h1>{Publish_Date}</h1>
+    const { id, author_name, blog_title, Blog_cover_image, Author_image, Read_time, Publish_Date } = props.b;
 
-            <img src='{Blog_cover_image}'></img>
-            <img src='{Author_image}'></img>
+    return (
+
+        <div className='blog'>
+            {/* cover image */}
+            <img src={Blog_cover_image} id="cover-image" alt="" />
+
+            {/* body */}
+            <div className='body'>
+                {/* author intro */}
+                <div className='intro'>
+                    <div className='pic-name'>
+                        <img src={Author_image} alt="" />
+                        <div className='name'>
+                            <h4> {author_name} </h4>
+                            <p>{Publish_Date}</p>
+                        </div>
+                    </div>
+
+                    <div className='reading'>
+                        <span> {Read_time} read</span> &nbsp;  <button>
+                            <FontAwesomeIcon icon={faBookmark} />
+                        </button>
+                    </div>
+                </div>
+
+                {/* blog title */}
+                <div className='title'>
+                    <h1>{blog_title}</h1>
+                </div>
+
+                {/* #-trend */}
+
+                <div className='hash-trend'>
+                    <div># beginner</div>
+                    <div># Programming</div>
+                </div>
+
+                {/* mark read */}
+                <div className='mark-read'>
+                    Mark as read
+                </div>
+            </div>
+
+
         </div>
     );
 };
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -14,14 +14,15 @@ const Blogs = () => {
     return (
         <div className='blogs-bookmark'>
 
-            <div className='blogs'>
-               {
-                 blogs.map(blog =>
-                     <Blog key = {blog.id} 
-                     blog = {blog}
-                     
-                     ></Blog>)
-               }
+            <div className='blogs-container'>
+                {
+                    blogs.map(blog =>
+                        <Blog
+                            key={blog.id}
+                            b={blog}
+                        ></Blog>
+                    )
+                }
             </div>
 
             <div className='bookmark'>
